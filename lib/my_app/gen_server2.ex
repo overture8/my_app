6ef2297816736f0f174f76e4792dd9895ece2818
Phil McClure <phil.mcclure@gmail.com>
2017-08-31 09:46:48 +0100

    Update gen_server2.ex

--- lib/my_app/gen_server2.ex
+++ lib/my_app/gen_server2.ex
@@ -9,7 +9,7 @@ defmodule MyApp.GenServer2 do
     GenServer.call(__MODULE__, :slow_thing)
   end 
 
-	def handle_call(:slow_thing, _from, state) do
+  def handle_call(:slow_thing, _from, state) do
     :timer.sleep(10000)
     {:reply, "Some return value", state}
   end
